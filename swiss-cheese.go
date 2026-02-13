d05e53c8660edc5c89cd751e6472b756d16b0f63
Henry <hwodza@gmail.com>
2026-02-13 18:07:23 -0500

    made a generate_line func that will take the input (and eventually combined with the cheese) and print

--- swiss-cheese.go
+++ swiss-cheese.go
@@ -6,16 +6,28 @@ import (
 	"os"
 )
 
+func generate_line(inputCH chan string) {
+	for i := range inputCH {
+		fmt.Println(i)
+	}
+}
+
 func main() {
 	file, err := os.Open("lorem.txt")
 	if err != nil {
 		panic(err)
 	}
 
+	inputCH := make(chan string)
+
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
+	go func() {
+		for scanner.Scan() {
+			inputCH <- scanner.Text()
+		}
+		close(inputCH)
+	}()
 
+	generate_line(inputCH)
 }
